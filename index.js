8454a173554daf58276385158d5ea6659778e2bc
Lewis Odero <lewisodero27@gmail.com>
2024-07-24 09:26:29 +0300

    test: pass arithmetic tests in js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+
+//multiply
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+//random
+let random = Math.floor(Math.random() * 10) + 1;
+
+//mod
+
+let num3 = 19
+let num4 = 5
+let mod = num3 % num4
+
+//max
+let max = Math.max(1, 2, 3, 4, 20)
+
+
